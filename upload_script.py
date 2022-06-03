7e8ae202fd051e9b935c1019e514d97c2b194d9a
Adrián Mudarra Martín <adrianmudarramartin@gmail.com>
2022-06-03 13:04:09 +0200

    Update

--- upload_script.py
+++ upload_script.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import ctypes, sys
 
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+	print('PERMISOS DE ADMINISTRADOR COMPROBADOS')
+	pass
+else:
+	print('NECESITO DARLE PERMISOS')
+	ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+	sys.exit()
+
 
 app = Flask(__name__)
-app.config['IMAGE_UPLOADS'] = 'C:/Users/Usuario/Desktop/Proyectos/adrianmudarramartin.github.io/Images'
+app.config['IMAGE_UPLOADS'] = 'C:/Users/Usuario/Desktop'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -22,4 +36,4 @@ def fajardo():
 	return "Puto Fajardo qué buen coche se ha comprado"
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(port=5001)
