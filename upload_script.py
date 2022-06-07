f3888f43f110a18043b20f25fd2f19ac49140437
adrianmudarramartin <adrianmudarramartin@gmail.com>
2022-06-07 23:18:55 +0200

    Updated

--- upload_script.py
+++ upload_script.py
@@ -1,24 +1,9 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-import ctypes, sys
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.chdir("..")
-
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
-
-if is_admin():
-	print('PERMISOS DE ADMINISTRADOR COMPROBADOS')
-	pass
-else:
-	print('NECESITO DARLE PERMISOS')
-	ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-	sys.exit()
 
 
 app = Flask(__name__)
