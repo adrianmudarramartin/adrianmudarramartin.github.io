589a822433dc32266d1136024473b4ff6be700d5
Adrián Mudarra Martín <adrianmudarramartin@gmail.com>
2022-06-15 09:56:21 +0200

    He hecho cosas como esto y lo otro

--- upload_script.py
+++ upload_script.py
@@ -1,43 +1,73 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+import asana
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.chdir("..")
-credentials_dict = {'AMM': '1234'}
+credentials_AMM = {'Username': 'AMM', 'Password': '1234', 'Tier': 1}
+credentials_AFM = {'Username': 'AFM', 'Password': '5678', 'Tier': 0}
+credentials_list = [credentials_AMM, credentials_AFM]
 
 app = Flask(__name__)
 app.config['FILE_UPLOADS'] = 'C:/Users/Usuario/Desktop/Proyectos/adrianmudarramartin.github.io/Files'
+
+client = asana.Client.access_token('1/1202152890606392:57a2152dee423466ce283d2b8a3c70f5')
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
 	if request.method == "GET":
 		return render_template("iniciarsesion.htm")
 	if request.method == "POST" and request.files.getlist('file') == []:
-		pass
 		username = request.form.get('Usuario')
 		password = request.form.get('Contraseña')
 		try:
-			if credentials_dict[username] == password:
-				return render_template("webpage.htm")
-			else:
-				return render_template("iniciarsesion.htm")
+			credentials_check = False
+			for credentials_dict in credentials_list:
+				if credentials_dict['Username'] == username and credentials_dict['Password'] == password:
+					credentials_check = True
+					return render_template("webpage.htm")
+					break
+			if credentials_check == False: return render_template("iniciarsesion.htm")
 		except:
 			return render_template("iniciarsesion.htm")
 			
 	elif request.method == "POST" and request.files.getlist('file') != []:
-		#print(request.files)
-		#print(request.files.getlist('file'))
 		print(request.form.get('type'))
 		for file in request.files.getlist('file'):
-			#print(file)
 			filename = secure_filename(file.filename)
 			file.save(app.config['FILE_UPLOADS']+'/'+filename)
 	return render_template("webpage.htm")
 
-@app.route("/Fajardo")
-def fajardo():
-	return "Puto Fajardo qué buen coche se ha comprado"
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
+	if request.method == "GET":
+		return render_template("iniciarsesion.htm")
+	if request.method == "POST":
+		username = request.form.get('Usuario')
+		password = request.form.get('Contraseña')
+		try:
+			credentials_check = False
+			for credentials_dict in credentials_list:
+				if credentials_dict['Username'] == username and credentials_dict['Password'] == password:
+					credentials_check = True
+					workspaces = client.workspaces.find_all()
+					workspace = list(workspaces)[0]
+					projects = client.projects.find_all({'workspace': workspace['gid']})
+					plant_list = []
+					for project in projects:
+						plant_list.append(project['name'])
+					return render_template("admin.htm", plant_list=plant_list)
+					break
+			if credentials_check == False: return render_template("iniciarsesion.htm")
+		except:
+			return render_template("iniciarsesion.htm")
 
 if __name__ == "__main__":
 	app.run(port=5000)
+
+
+
+
+
+print(project['name'])
