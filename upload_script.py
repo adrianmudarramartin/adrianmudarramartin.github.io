b42be8f5095768769a837a4c4afd8d9a6e54d511
Adrián Mudarra Martín <adrianmudarramartin@gmail.com>
2022-06-23 13:09:01 +0200

    Update

--- upload_script.py
+++ upload_script.py
@@ -5,7 +5,6 @@ import sqlite3
 import asana
 import re
 import os
-import json
 from datetime import datetime
 
 con = sqlite3.connect('Database.db', check_same_thread=False)
@@ -24,7 +23,7 @@ client = asana.Client.access_token('1/1202152890606392:57a2152dee423466ce283d2b8
 @app.route("/", methods=['GET', 'POST'])
 def home():
 	if request.method == "GET":
-		return render_template("iniciarsesion.htm")
+		return render_template("iniciarsesion.htm", admin=False)
 	if request.method == "POST" and request.files.getlist('Archivo') == []:
 		username = request.form.get('Usuario')
 		password = request.form.get('Contraseña')
@@ -40,11 +39,11 @@ def home():
 					if '\''+str(credentials_tuple[0])+'\'' in asignee_list: 
 						project_list.append(project_tuple[1])
 				return render_template("webpage.htm", username=username, project_list=project_list)
-			else: return render_template("iniciarsesion.htm", info='No tiene permisos de acceso')
+			else: return render_template("iniciarsesion.htm", info='No tiene permisos de acceso', admin=False)
 		except Exception as e:
-			return render_template("iniciarsesion.htm", info='Usuario o contraseña incorrectos. Inténtelo de nuevo')
-			
-	elif request.method == "POST" and request.files.getlist('Archivo') != []:
+			return render_template("iniciarsesion.htm", info='Usuario o contraseña incorrectos. Inténtelo de nuevo', admin=False)
+			 
+	elif request.method == "POST" and request.form['submit'] == "Iniciar sesión":
 		plant = request.form.get('Planta')
 		username = request.form.get('Usuario')
 		cursor.execute('''SELECT ID_Usuario FROM Users WHERE Usuario = ?''', (username,))
@@ -55,17 +54,29 @@ def home():
 			os.mkdir(app.config['FILE_UPLOADS']+'\\'+plant+'\\'+type)
 		elif not os.path.exists(os.path.dirname(os.path.abspath(__file__))+'\\Files\\'+plant+'\\'+type):
 			os.mkdir(app.config['FILE_UPLOADS']+'\\'+plant+'\\'+type)
+		now = datetime.now().strftime("%d-%m-%Y %H.%M")
+		number = 0
 		for file in request.files.getlist('Archivo'):
-			filename = secure_filename(file.filename)
-			file.save(app.config['FILE_UPLOADS']+plant+'\\'+type+'\\'+filename)
-	return render_template("iniciarsesion.htm", info='Archivos subidos correctamente')
+			filetype = file.filename.split('.')[-1]
+			#filename = secure_filename(file.filename)
+			while(True):
+				filepath = app.config['FILE_UPLOADS']+'\\'+plant+'\\'+type+'\\'+now+'_'+username+'_'+type+'_'+str(number)+'.'+filetype  
+				if not os.path.exists(filepath):
+					file.save(app.config['FILE_UPLOADS']+'\\'+plant+'\\'+type+'\\'+now+'_'+username+'_'+type+'_'+str(number)+'.'+filetype)
+					number = number + 1
+					break
+				else: number = number + 1
+		if type == 'PARTES DE INSPECCIÓN': cursor.execute('''UPDATE Projects SET Partes = ?, Subido_Partes = ? WHERE Nombre = ?''', (1, user_id, plant))
+		else: cursor.execute('''UPDATE Projects SET Fotos = ?, Subido_Fotos = ? WHERE Nombre = ?''', (1, user_id, plant))
+		con.commit()
+		return render_template("iniciarsesion.htm", info='Archivos subidos correctamente', admin=False)
 
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
 	if request.method == "GET":
-		return render_template("iniciarsesion.htm")
+		return render_template("iniciarsesion.htm", admin=True)
 		
-	if request.method == "POST" and request.form.get('Planta') == None:
+	if request.method == "POST" and request.form['submit'] == 'Abrir caso':
 		username = request.form.get('Usuario')
 		password = request.form.get('Contraseña')
 		try:
@@ -86,10 +97,10 @@ def admin():
 				asignee_list = []
 				for users_tuple in users_tuples_list:
 					asignee_list.append(users_tuple)
-				return render_template("admin.htm", plant_list=sorted(plant_list), username=username, asignee_list=sorted(asignee_list))
-			else: return render_template("iniciarsesion.htm", info='Usuario o contraseña incorrectos. Inténtelo de nuevo')
+				return render_template("admin-open.htm", plant_list=sorted(plant_list), username=username, asignee_list=sorted(asignee_list))
+			else: return render_template("iniciarsesion.htm", info='Usuario o contraseña incorrectos. Inténtelo de nuevo', admin=True)
 		except Exception as e:
-			return render_template("iniciarsesion.htm", info='Usuario o contraseña incorrectos. Inténtelo de nuevo')
+			return render_template("iniciarsesion.htm", info='Usuario o contraseña incorrectos. Inténtelo de nuevo', admin=True)
 	
 	elif request.method == "POST" and request.form.get('Planta') != None and request.form.get('Usuario') != None:
 		plant = request.form.get('Planta')
@@ -100,7 +111,18 @@ def admin():
 		now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 		cursor.execute('''INSERT INTO Projects (Nombre, Creador, Asignado, Fecha_Apertura, Fotos, Partes, Subido_Fotos, Subido_Partes, Finalizado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', (plant, user_id, str(asignee_list), now, 0, 0, None, None, 0))
 		con.commit()
-		return render_template("iniciarsesion.htm", info='Proyecto abierto correctamente')
+		return render_template("iniciarsesion.htm", info='Proyecto abierto correctamente', admin=True)
 
+	if request.method == "POST" and request.form['submit'] == 'Revisar caso':
+		username = request.form.get('Usuario')
+		password = request.form.get('Contraseña')
+		try:
+			cursor.execute('''SELECT ID_Usuario, Clave, Tier FROM Users WHERE Usuario = ?''', (username,))
+			credentials_tuple = cursor.fetchone()
+			if credentials_tuple[1] == password and credentials_tuple[2] >= 1:
+				cursor.execute('''SELECT ID_Proyecto, Nombre FROM Projects''')
+				
+		except Exception as e:
+			return render_template("iniciarsesion.htm", info='Usuario o contraseña incorrectos. Inténtelo de nuevo', admin=True)
 if __name__ == "__main__":
 	app.run(port=5000)
